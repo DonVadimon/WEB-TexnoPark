6ea21cb5259b2bd662055bd9d6ab86d3fb106613
Vadim Khizhnyakov <provcydia@gmail.com>
2020-10-25 01:26:41 +0300

    Page answers.html was rewrite for base.html Fuck dJango reload page. Why I should reload a source file *.css to see changes?

--- app/views.py
+++ app/views.py
@@ -11,25 +11,9 @@ def ask_question(request):
     #Page for create new Question
     return render(request, 'create_question.html', {})
 
-
-questions = [
-    {
-        'id': idx,
-        'title': f'Question number {idx}',
-        'text': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nisi numquam aliquid dignissimos repudiandae sint, porro dolores, qui quod cumque dolorem. Id recusandae hic a quae illo excepturi quia similique',
-    } for idx in range(10)
-]
-
 def answers(request, id):
     #Page with answers on current question
-    
-    question = questions[id]
-    return render(request, 'question_page.html', {
-        'question': question
-    })
-    
-    #return render(request, 'answers.html', {})
-
+    return render(request, 'answers.html', {})
 
 def tag_questions(request):
     #Page with question on one tag
